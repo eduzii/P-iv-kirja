c7abccb0874d5bc0834688aece426ba084239352
eduzii <eetuauvinen@hotmail.com>
2023-12-15 20:33:46 +0200

    Tekstin syötölle boksi. Vielä ei pysty syöttämään tekstiä sivulle vaan tulee error.

--- Diary/demo/src/main/java/diary/Home.java
+++ Diary/demo/src/main/java/diary/Home.java
@@ -2,9 +2,10 @@ package diary;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
-public class home {
+public class Home {
     @GetMapping("/")
-    public String koti(){
+    public String koti()
+    {
         return "home";
     }
 }
